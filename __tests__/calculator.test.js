7650f32de0f073338e7bca71e81a60acf701d023
Woo Jin Kim <kimwoojin211@gmail.com>
2021-02-05 11:12:04 -0800

    fail Mars life Expectancy tests

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -1,8 +1,4 @@
-//woojin notes:
-//wait. have i been thinking of this wrong.
-//for realAge(age), age is given in earth years, and you want to return the planetary age.
-//lifeexpectancy, i also want it to return the planetary age.
-//god damn it. i had the life expectancy formula right the first time. 
+//woojin notes: 
 
 import Calculator from './../src/calculator.js';
 // assumes for all functions that all ages are integers that round down after conversion
@@ -45,5 +41,9 @@ describe('Calculator', () => {
     expect(calculator.lifeExpectancy(26, "Venus")).toEqual(70);
     expect(calculator.lifeExpectancy(100, "Venus")).toEqual(-48);
   }); 
-
+  test("It will determine how many more years one is expected to live on Mars (or how many years past the average life expectancy one has lived", () => {
+    expect(calculator.lifeExpectancy(26, "Mars")).toEqual(82);
+    expect(calculator.lifeExpectancy(100, "Mars")).toEqual(-18);
+  }); 
+  
 });
