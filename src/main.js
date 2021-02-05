3fdb60e61bfc4caa5b5754773bec648164ee002f
Woo Jin Kim <kimwoojin211@gmail.com>
2021-02-05 15:45:07 -0800

    prevent number form from submitting (troubleshooting)

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-//import $ from 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
@@ -6,3 +6,11 @@ import Calculator from './calculator';
 
 let calculator = new Calculator();
 console.log(calculator);
+
+$(function(){
+
+  $("form").on("submit",function(event){
+      event.preventDefault();
+  });
+
+});
