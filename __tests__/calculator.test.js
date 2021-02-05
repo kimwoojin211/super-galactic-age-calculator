0e011b874ee0f10b25865987b6b1adcf1c398ca7
Woo Jin Kim <kimwoojin211@gmail.com>
2021-02-05 09:11:15 -0800

    fail constructor test

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -1,10 +1,9 @@
 import Calculator from './../src/calculator.js';
 
 describe('Calculator', () => {
-  let Calculator;
-
-  test("It will create a new Super Galactic Age Calculator object", () =>{
-    calculator = new Calculator();
-    expect(calculator.toEqual(undefined));
+  let calculator;
+test("It will create a new Super Galactic Age Calculator object", () =>{
+  calculator = new Calculator();
+  expect(calculator).toEqual();
   });
 });
--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
-// import $ from 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import Calculator from './calculator';
+
+console.log(calculator = new Calculator());
