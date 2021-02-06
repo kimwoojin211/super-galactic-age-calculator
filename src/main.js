d1f5ae3455151fddf7d3386418fd09e846668157
Woo Jin Kim <kimwoojin211@gmail.com>
2021-02-05 16:54:50 -0800

    add template literals to main.js

--- src/main.js
+++ src/main.js
@@ -11,19 +11,22 @@ function attachButtonListener() {
     const input = parseInt($("#userAge").val());
     console.log(input);
     if (Number.isInteger(input) && input > 0) {
-      const lifeExp = calculator.lifeExpectancy(input, this.id);
+      let lifeExp = calculator.lifeExpectancy(input, this.id);
       const planetAge = calculator.planetAge(input, this.id);
       $(".result").show();
       $("#planet").text(this.id);
       $("#planetAge").text(planetAge + (planetAge > 1 ? " years" : " year"));
 
       if (lifeExp >= 0) {
-        $("#lifeExp").text("You have " + lifeExp + " more years to live.");
+        $("#lifeExp").text(`You have ${lifeExp} more years to live.`);
         if (lifeExp === 0)
           $("#lifeExp").append(" Yikes.");
       }
       else
-        $("#lifeExp").text("Congratulations! You have lived  " + (-(lifeExp)) + " years more than we expected.");
+      {
+        lifeExp = -lifeExp
+        $("#lifeExp").text(`Congratulations! You have lived ${lifeExp} years more than we expected.`);
+      }
     }
   });
 }
